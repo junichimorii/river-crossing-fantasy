c81c3b21b59059c79b1f23eb11c195faff068a98
js8jp <junichi.morii.4@gmail.com>
2024-02-28 09:38:00 +0900

    2024-02-28

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,6 @@
 // Composables
 import { createRouter, createWebHashHistory } from 'vue-router'
-import type { RouteRecordRaw } from 'vue-router'
-import Default from '@/layouts/default.vue'
-import Home from '@/pages/home.vue'
-import List from '@/pages/list.vue'
-import Scene from '@/pages/scene.vue'
-import Scenes from '@/pages/scenes.vue'
-
-const routes: Array<RouteRecordRaw> = [{
-  path: '/',
-  redirect: '/home',
-  component: Default,
-  children: [{
-    path: '/home',
-    component: Home,
-  }, {
-    path: '/scenes',
-    component: Scenes,
-  }, {
-    path: '/:id(\\d+)',
-    component: Scene,
-  }, {
-    path: '/list',
-    component: List,
-  }]
-}]
+import routes from './routes'
 
 const router = createRouter({
   history: createWebHashHistory(),
---
+++ src/router/routes.ts
@@ -0,0 +1,27 @@
+import type { RouteRecordRaw } from 'vue-router'
+import Default from '@/layouts/default.vue'
+import Home from '@/pages/home.vue'
+import List from '@/pages/list.vue'
+import Scene from '@/pages/scene.vue'
+import Scenes from '@/pages/scenes.vue'
+
+const routes: Array<RouteRecordRaw> = [{
+  path: '/',
+  redirect: '/home',
+  component: Default,
+  children: [{
+    path: '/home',
+    component: Home,
+  }, {
+    path: '/scenes',
+    component: Scenes,
+  }, {
+    path: '/:id(\\d+)',
+    component: Scene,
+  }, {
+    path: '/list',
+    component: List,
+  }]
+}]
+
+export default routes
