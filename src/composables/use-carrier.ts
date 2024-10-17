1b2c2c3a8ef045a88fc11a63f7abbb099e78fd34
junmolly <junichi.morii.4@gmail.com>
2024-10-17 19:11:31 +0900

    2024-10-17

--- src/composables/use-carrier.ts
+++ src/composables/use-carrier.ts
@@ -12,10 +12,17 @@ const useCarrier = (
   const { boarding, diseased } = useCastState(state)
   const { isRower, getWeight, getDuration } = useCast()
 
-  /** 乗り物に乗っている登場人物 */
-  const getPassengers = (
-    carrier: Carrier
-  ) => scene.value.casts.filter(cast => boarding(cast) === carrier.id)
+  /** 乗り物の行先を算出 */
+  const getDestination = (
+    carrier: Carrier,
+    bound: Bound,
+  ) => scene.value.landscape?.island
+    ? coord(carrier) === 0
+      ? bound === 'inbound'
+        ? 1
+        : -1
+      : 0
+    : -coord(carrier)
 
   /** 対岸までの所要時間を算出 */
   const getElapsedTime = (
@@ -29,22 +36,32 @@ const useCarrier = (
     carrier: Carrier
   ) => getPassengers(carrier).reduce((weight, cast) => weight + getWeight(cast), 0)
 
-  /** 乗り物の行先を算出 */
-  const getDestination = (
-    carrier: Carrier,
-    bound: Bound,
-  ) => scene.value.landscape?.island
-    ? coord(carrier) === 0
-      ? bound === 'inbound'
-        ? 1
-        : -1
-      : 0
-    : -coord(carrier)
+  /** 乗り物に乗っている登場人物 */
+  const getPassengers = (
+    carrier: Carrier
+  ) => scene.value.casts.filter(cast => boarding(cast) === carrier.id)
+
+  /** 乗り物に乗っている筏を漕げる登場人物 */
+  const getRowers = (
+    carrier: Carrier
+  ) => getPassengers(carrier).filter(cast =>
+    // 聖女が同乗しているか
+    getPassengers(carrier).some(cast => cast.role.saint)
+      // 聖女が同乗している場合、筏を漕げる人物全員
+      ? isRower(cast)
+      // 聖女が同乗していない場合、筏を漕げる状態異常でない人物全員
+      : isRower(cast) && !diseased(cast)
+  )
 
   /** 乗員がいるかどうか */
   const hasPassengers = (
     carrier: Carrier
   ) => getPassengers(carrier).length > 0
+
+  /** 操作可能かどうか */
+  const isOperable = (
+    carrier: Carrier
+  ) => getRowers(carrier).length > 0 && getLoad(carrier) <= carrier.capacity
 
   /** 空席があるかどうか */
   const isVacancy = (
@@ -52,24 +69,13 @@ const useCarrier = (
     cast: Cast,
   ) => (getLoad(carrier) + getWeight(cast)) <= carrier.capacity
 
-  /** 操作可能かどうか */
-  const isOperable = (
-    carrier: Carrier
-  ) => (
-    // 筏を漕げる乗員がおり、かつ状態異常でない
-    getPassengers(carrier).some(cast => isRower(cast) && !diseased(cast))
-    ||
-    // 筏を漕げる乗員がおり、かつ聖女も同乗している
-    (getPassengers(carrier).some(cast => isRower(cast)) && getPassengers(carrier).some(cast => cast.role.saint))
-  ) && getLoad(carrier) <= carrier.capacity
-
   return {
+    getDestination,
     getElapsedTime,
     getLoad,
-    getDestination,
     hasPassengers,
-    isVacancy,
     isOperable,
+    isVacancy,
   }
 }
 export default useCarrier
